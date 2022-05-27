8f46e3bfbff0586ccec4b6bf2347b06686197c03
mateus schwarz <mateus.cortiano@gmail.com>
2022-05-26 21:23:16 -0300

    - clean up

--- scripts/build.js
+++ scripts/build.js
@@ -4,32 +4,37 @@ const fs = require('fs')
 
 // ---
 
+const TITLE = 'hide youtube theater navbar'
 const OUTPUT_FILE = './dist/bundle.html'
 
 // ---
 
+let buffer = `<!-- ${TITLE} -->\r\n\r\n`
+
 fs.readFile('./src/script.js', 'utf8', (err, data) => {
   if (err) throw err
-
-  let buffer = '<!-- hide youtube theater nav bar -->\r\n\r\n'
 
   buffer += '<script>\r\n'
 
   for (let line of data.split('\r\n')) buffer += '  ' + line + '\r\n'
 
   buffer += '</script>\r\n\r\n\r\n'
+})
 
-  fs.readFile('./src/styles.css', 'utf8', (err, data) => {
+fs.readFile('./src/styles.css', 'utf8', (err, data) => {
+  if (err) throw err
+
+  buffer += '<style>\r\n'
+
+  for (let line of data.split('\r\n')) buffer += '  ' + line + '\r\n'
+
+  buffer += '</style>\r\n'
+})
+
+fs.readFile(OUTPUT_FILE, 'utf8', err => {
+  if (err) throw err
+
+  fs.writeFile(OUTPUT_FILE, buffer, 'utf8', err => {
     if (err) throw err
-
-    buffer += '<style>\r\n'
-
-    for (let line of data.split('\r\n')) buffer += '  ' + line + '\r\n'
-
-    buffer += '</style>\r\n'
-
-    fs.writeFile(OUTPUT_FILE, buffer, 'utf8', err => {
-      if (err) throw err
-    })
   })
 })
