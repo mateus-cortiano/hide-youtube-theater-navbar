2adfb3f55ed2a4c4e809ee4ae29b1e9492961055
mateus schwarz <mateus.cortiano@gmail.com>
2022-05-27 00:23:14 -0300

    - added jquery cdn

--- scripts/build.js
+++ scripts/build.js
@@ -6,10 +6,12 @@ const fs = require('fs')
 
 const TITLE = 'hide youtube theater navbar'
 const OUTPUT_FILE = './dist/bundle.html'
+const JQUERY_CDN =
+  '<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>'
 
 // ---
 
-let buffer = `<!-- ${TITLE} -->\r\n\r\n`
+let buffer = `<!-- ${TITLE} -->\r\n\r\n${JQUERY_CDN}\r\n\r\n`
 
 fs.readFile('./src/script.js', 'utf8', (err, data) => {
   if (err) throw err
